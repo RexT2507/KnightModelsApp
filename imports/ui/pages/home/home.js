ec2132e696e761794e7cfd3764a28ee6206b837a
RexT2507 <baptiste.marmier@etu.univ-amu.fr>
2019-03-07 11:55:53 +0100

    Nettoyage des fichiers du projet

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -2,8 +2,3 @@ import './home.html';
 
 import '../../components/menu/menu.js';
 import '../../components/personnages/personnages.js';
-
-
-// Anciens exemple
-import '../../components/hello/hello.js';
-import '../../components/info/info.js';
