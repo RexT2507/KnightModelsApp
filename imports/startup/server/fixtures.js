85790484cb1d5bbe48f03740b760525399907b81
RexT2507 <baptiste.marmier@etu.univ-amu.fr>
2019-03-14 19:07:26 +0100

    Ajout des images dans la BDD et affichages dans les cards

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -10,26 +10,32 @@ Meteor.startup(() =>
         const data = 
         [
             {
+                img: '/img/Batman_Arkham.PNG',
                 alias: 'Batman (Arkham Knight)',
                 cout: 150,
             },
             {
+                img: '/img/Joker.PNG',
                 alias: 'Joker (Arkham Asylum)',
                 cout: 105,
             },
             {
+                img: '/img/Batman_Who_Laught.PNG',
                 alias: 'The Batman Who Laughs',
                 cout: 140,
             },
             {
+                img: '/img/Heavy_Trooper.PNG',
                 alias: 'LexCorp Heavy Trooper',
                 cout: 55,
             },
             {
+                img: '/img/Catwoman.PNG',
                 alias: 'Catwoman (Modern Age)',
                 cout: 69,
             },
             {
+                img: '/img/Bane.PNG',
                 alias: 'Bane (Rebirth)',
                 cout: 161,
             },
