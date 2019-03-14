4a9bf95cb17d436263bbcd14619c4abcdd463bb1
RexT2507 <baptiste.marmier@etu.univ-amu.fr>
2019-03-14 22:35:54 +0100

    Mise à jours des cards

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -11,31 +11,37 @@ Meteor.startup(() =>
         [
             {
                 img: '/img/Batman_Arkham.PNG',
+                categori: 'The Brave And The Bold',
                 alias: 'Batman (Arkham Knight)',
                 cout: 150,
             },
             {
                 img: '/img/Joker.PNG',
+                categori: 'Joker',
                 alias: 'Joker (Arkham Asylum)',
                 cout: 105,
             },
             {
                 img: '/img/Batman_Who_Laught.PNG',
+                categori: 'Agent libre',
                 alias: 'The Batman Who Laughs',
                 cout: 140,
             },
             {
                 img: '/img/Heavy_Trooper.PNG',
+                categori: 'Agent libre',
                 alias: 'LexCorp Heavy Trooper',
                 cout: 55,
             },
             {
                 img: '/img/Catwoman.PNG',
+                categori: 'Organized crime',
                 alias: 'Catwoman (Modern Age)',
                 cout: 69,
             },
             {
                 img: '/img/Bane.PNG',
+                categori: 'Bane',
                 alias: 'Bane (Rebirth)',
                 cout: 161,
             },
