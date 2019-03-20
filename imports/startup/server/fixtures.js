f270c9a95be95e06346d6916b6cb40f0031f3c16
RexT <baptiste.marmierskyrm@hotmail.fr>
2019-03-20 15:45:50 +0100

    Nettoyage du fichier fixtures et ajout des images en base64 dans le fichier JSON

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -7,55 +7,12 @@ Meteor.startup(() =>
     // Si la collection Personnages est vide
     if(Personnages.find().count() === 0)
     {
-        // const data = 
-        // [
-        //     {
-        //         img: '/img/Batman_Arkham.PNG',
-        //         categori: 'The Brave And The Bold',
-        //         alias: 'Batman (Arkham Knight)',
-        //         cout: 150,
-        //     },
-        //     {
-        //         img: '/img/Joker.PNG',
-        //         categori: 'Joker',
-        //         alias: 'Joker (Arkham Asylum)',
-        //         cout: 105,
-        //     },
-        //     {
-        //         img: '/img/Batman_Who_Laught.PNG',
-        //         categori: 'Agent libre',
-        //         alias: 'The Batman Who Laughs',
-        //         cout: 140,
-        //     },
-        //     {
-        //         img: '/img/Heavy_Trooper.PNG',
-        //         categori: 'Agent libre',
-        //         alias: 'LexCorp Heavy Trooper',
-        //         cout: 55,
-        //     },
-        //     {
-        //         img: '/img/Catwoman.PNG',
-        //         categori: 'Organized crime',
-        //         alias: 'Catwoman (Modern Age)',
-        //         cout: 69,
-        //     },
-        //     {
-        //         img: '/img/Bane.PNG',
-        //         categori: 'Bane',
-        //         alias: 'Bane (Rebirth)',
-        //         cout: 161,
-        //     },
-        // ];
-
-        // data.forEach(personnage => Personnages.insert(personnage));
-
+        // Le fichier JSON de la collection se trouve dans le dossier private
         const data = JSON.parse(Assets.getText("knight_model_app_collection.json"));
 
         data.forEach(function (personnage) {
             Personnages.insert(personnage);
-        })
-
-       
+        })   
 
     }
 
