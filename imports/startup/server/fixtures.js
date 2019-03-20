c611a8087d1b7e7f0ccdd0d05b78dfb1208bcbe6
RexT <baptiste.marmierskyrm@hotmail.fr>
2019-03-20 10:28:11 +0100

    Ajout du fichier JSON et de l'encodage en Base64

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -7,47 +7,55 @@ Meteor.startup(() =>
     // Si la collection Personnages est vide
     if(Personnages.find().count() === 0)
     {
-        const data = 
-        [
-            {
-                img: '/img/Batman_Arkham.PNG',
-                categori: 'The Brave And The Bold',
-                alias: 'Batman (Arkham Knight)',
-                cout: 150,
-            },
-            {
-                img: '/img/Joker.PNG',
-                categori: 'Joker',
-                alias: 'Joker (Arkham Asylum)',
-                cout: 105,
-            },
-            {
-                img: '/img/Batman_Who_Laught.PNG',
-                categori: 'Agent libre',
-                alias: 'The Batman Who Laughs',
-                cout: 140,
-            },
-            {
-                img: '/img/Heavy_Trooper.PNG',
-                categori: 'Agent libre',
-                alias: 'LexCorp Heavy Trooper',
-                cout: 55,
-            },
-            {
-                img: '/img/Catwoman.PNG',
-                categori: 'Organized crime',
-                alias: 'Catwoman (Modern Age)',
-                cout: 69,
-            },
-            {
-                img: '/img/Bane.PNG',
-                categori: 'Bane',
-                alias: 'Bane (Rebirth)',
-                cout: 161,
-            },
-        ];
+        // const data = 
+        // [
+        //     {
+        //         img: '/img/Batman_Arkham.PNG',
+        //         categori: 'The Brave And The Bold',
+        //         alias: 'Batman (Arkham Knight)',
+        //         cout: 150,
+        //     },
+        //     {
+        //         img: '/img/Joker.PNG',
+        //         categori: 'Joker',
+        //         alias: 'Joker (Arkham Asylum)',
+        //         cout: 105,
+        //     },
+        //     {
+        //         img: '/img/Batman_Who_Laught.PNG',
+        //         categori: 'Agent libre',
+        //         alias: 'The Batman Who Laughs',
+        //         cout: 140,
+        //     },
+        //     {
+        //         img: '/img/Heavy_Trooper.PNG',
+        //         categori: 'Agent libre',
+        //         alias: 'LexCorp Heavy Trooper',
+        //         cout: 55,
+        //     },
+        //     {
+        //         img: '/img/Catwoman.PNG',
+        //         categori: 'Organized crime',
+        //         alias: 'Catwoman (Modern Age)',
+        //         cout: 69,
+        //     },
+        //     {
+        //         img: '/img/Bane.PNG',
+        //         categori: 'Bane',
+        //         alias: 'Bane (Rebirth)',
+        //         cout: 161,
+        //     },
+        // ];
 
-        data.forEach(personnage => Personnages.insert(personnage));
+        // data.forEach(personnage => Personnages.insert(personnage));
+
+        const data = JSON.parse(Assets.getText("knight_model_app_collection.json"));
+
+        data.forEach(function (personnage) {
+            Personnages.insert(personnage);
+        })
+
+       
 
     }
 
